f2d2fb65393c39b7ea78f65091568a1e877dfb8c
Abin-4 <161850237+Abin-4@users.noreply.github.com>
2025-07-29 00:53:33 +0530

    Update serverUrl.js url change

--- src/Server/serverUrl.js
+++ src/Server/serverUrl.js
@@ -1,4 +1,4 @@
-export const SERVER_URL= "https://server-sand-qenh.onrender.com"
+export const SERVER_URL= "https://server-sandmmss.onrender.com"
 
 //export const SERVER_URL= "http://localhost:3005"
  
